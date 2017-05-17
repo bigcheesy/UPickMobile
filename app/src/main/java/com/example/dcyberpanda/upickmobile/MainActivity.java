eb88fce4a6b7c597f7fa2b092051efd135da12bf
bigcheesy <damianndoja1@gmail.com>
2017-05-17 22:51:05 +0200

    Think I added stuff

---
+++ app/src/main/java/com/example/dcyberpanda/upickmobile/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.dcyberpanda.upickmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    public void click(View v){
+        Toast.makeText(this,"click",Toast.LENGTH_SHORT);
+    }
+}
