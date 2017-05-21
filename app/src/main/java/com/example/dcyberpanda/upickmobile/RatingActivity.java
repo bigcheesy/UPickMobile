9731f04704754aa5553d454e1fb0583c5b4a52a3
thebigk123 <kristian.shatraj.1@gmail.com>
2017-05-21 15:53:42 -0700

    ayylmao

---
+++ app/src/main/java/com/example/dcyberpanda/upickmobile/RatingActivity.java
@@ -0,0 +1,17 @@
+package com.example.dcyberpanda.upickmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RatingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rating);
+    }
+}
+
+
+
+
