c9a30661a13509abdea9b32e20856bc52a42119b
thebigk123 <kristian.shatraj.1@gmail.com>
2017-05-25 16:58:47 -0700

    ayylmaooooo

--- app/src/main/java/com/example/dcyberpanda/upickmobile/MainActivity.java
+++ app/src/main/java/com/example/dcyberpanda/upickmobile/MainActivity.java
@@ -30,5 +30,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, RatingActivity.class);
         startActivity(intent);
         finish();
+
+
     }
 }
