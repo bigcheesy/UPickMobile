59d29abcf418e93c4df783c394a885d3960256e2
thebigk123 <kristian.shatraj.1@gmail.com>
2017-05-29 19:07:28 -0700

    ayyyylmaokai

--- app/src/main/java/com/example/dcyberpanda/upickmobile/RatingActivity.java
+++ app/src/main/java/com/example/dcyberpanda/upickmobile/RatingActivity.java
@@ -1,17 +1,30 @@
 package com.example.dcyberpanda.upickmobile;
 
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.example.dcyberpanda.upickmobile.CustomAdapters.CartAdapter;
 import com.example.dcyberpanda.upickmobile.CustomAdapters.RatingAdapter;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 public class RatingActivity extends AppCompatActivity {
 
     public static ArrayList<Rating> ratings;
+    ViewPager viewPager;
 
 
     @Override
@@ -19,11 +32,15 @@ public class RatingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
         ratings = new ArrayList<>();
+        viewPager = (ViewPager) findViewById(R.id.rating_viewpager);
+        viewPager.setAdapter(new CustomAdapter(getSupportFragmentManager(),getApplicationContext()));
         ratings.add(new Rating("Kristi", "Lokal qr", 5));
         ratings.add(new Rating("Dami", "Lokal eh", 3));
         ratings.add(new Rating("Samir", "SUPER APP DO E BLEJ PATJETER!!!!!!", 5));
-        ratings.add(new Rating("Fabio", "Hajde shkarkojm me tor", 1));
+        ratings.add(new Rating("Fabio", "Hajde e shkarkojm me tor", 1));
         createList();
+
+
     }
 
     private void createList(){
@@ -39,7 +56,36 @@ public class RatingActivity extends AppCompatActivity {
 
 
     }
+
+
+    private class CustomAdapter extends FragmentPagerAdapter {
+        int fragmentNumber = 2;
+        public CustomAdapter(FragmentManager supportFragmentManager, Context applicationContext) {
+            super(supportFragmentManager);
+
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return new Fragment1();
+                case 1:
+                    return new Fragment2();
+                default:
+                    return new Fragment1();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentNumber;
+        }
+
+
+    }
 }
+
 
 
 
