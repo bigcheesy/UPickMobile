fe925d57c872e5d4ce9929bc8309a3a746e0079d
thebigk123 <kristian.shatraj.1@gmail.com>
2017-06-05 17:32:01 -0700

    kajwdki

--- app/src/main/java/com/example/dcyberpanda/upickmobile/RatingActivity.java
+++ app/src/main/java/com/example/dcyberpanda/upickmobile/RatingActivity.java
@@ -5,12 +5,15 @@ import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -22,10 +25,15 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
+import static com.example.dcyberpanda.upickmobile.R.id.rating_viewpager;
+
 public class RatingActivity extends AppCompatActivity {
 
     public static ArrayList<Rating> ratings;
     ViewPager viewPager;
+    LinearLayout sliderDotspanel;
+    private int dotscount;
+    private ImageView[] dots;
 
 
     @Override
@@ -33,8 +41,50 @@ public class RatingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
         ratings = new ArrayList<>();
-        viewPager = (ViewPager) findViewById(R.id.rating_viewpager);
+        viewPager = (ViewPager) findViewById(rating_viewpager);
+
         viewPager.setAdapter(new CustomAdapter(getSupportFragmentManager(),getApplicationContext()));
+
+        CustomAdapter customAdapter = new CustomAdapter(getSupportFragmentManager(),this);
+        dotscount = customAdapter.getCount();
+        dots = new ImageView[dotscount];
+        sliderDotspanel = (LinearLayout) findViewById(R.id.SliderDots);
+
+        for(int i = 0;  i < dotscount; i++){
+
+            dots[i] = new ImageView(this);
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.pikjoaktive));
+            LinearLayout.LayoutParams params =  new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(8, 0, 8, 0);
+            sliderDotspanel.addView(dots[i], params);
+        }
+
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.pik));
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                for(int i = 0; i < dotscount; i++){
+
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.pikjoaktive));
+                }
+
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.pik));
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
         ratings.add(new Rating("Kristi", "Lokal qr", 5));
         ratings.add(new Rating("Dami", "Lokal eh", 3));
         ratings.add(new Rating("Samir", "SUPER APP DO E BLEJ PATJETER!!!!!!", 5));
